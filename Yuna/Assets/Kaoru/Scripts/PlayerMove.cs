a6e323aa069ed2f92cbc185550e5522d9537179d
riceman0000 <45267493+riceman0000@users.noreply.github.com>
2020-01-14 21:15:19 +0900

    TruckTestシーン、Player豆腐、UpdateFixedUpdateを一元管理する機能追加（拝借失礼）

---
+++ Yuna/Assets/Kaoru/Scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Yuna
+{
+    public class PlayerMove : ManagedMono
+    {
+        Rigidbody2D rb;
+        Vector2 vec = new Vector2(150, 0);
+        [SerializeField] int a;
+        [SerializeField] int b;
+        private void Start()
+        {
+            rb = this.gameObject.GetComponent<Rigidbody2D>();
+        }
+        public override void MUpdate()
+        {
+            a++;
+        }
+        public override void MFixedUpdate()
+        {
+            b++;
+        }
+    }
+}
