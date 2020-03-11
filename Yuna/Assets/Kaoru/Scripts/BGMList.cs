f959e5d057c94106d9b23afaf1f9d3de6129ecd8
riceman0000 <45267493+riceman0000@users.noreply.github.com>
2020-03-12 01:33:35 +0900

    Audio,SE,BGMの管理用ScritableObject作成(途中) 同じ内容で冗長なので動的にできたらやる予定

---
+++ Yuna/Assets/Kaoru/Scripts/BGMList.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable, CreateAssetMenu(fileName = "AudioList", menuName = "AudioList/Create")]
+
+public class BGMList : ScriptableObject
+{
+    public List<BGMContent> BGMContents = new List<BGMContent>();
+    Dictionary<BGMTag, AudioClip> BGMDics = new Dictionary<BGMTag, AudioClip>();
+
+    public Dictionary<BGMTag, AudioClip> BgmDics
+    {
+        get
+        {
+            if (BGMDics == null || BGMDics.Count == 0)
+            {
+                foreach (var item in BGMContents)
+                {
+                    BGMDics.Add(item.BGMName, item.BGMClip);
+                }
+            }
+            return BGMDics;
+        }
+
+    }
+}
+[System.Serializable]
+public class BGMContent
+{
+    public BGMTag BGMName;
+    public AudioClip BGMClip;
+}
+
+public enum BGMTag
+{
+    /*
+    //any tag
+    /// <summary>選択音</summary>
+    Select,
+    /// <summary>敵の発砲音</summary>
+    Shoot,
+    /// <summary>リザルト画面で鳴る音</summary>
+    Result
+    */
+};
