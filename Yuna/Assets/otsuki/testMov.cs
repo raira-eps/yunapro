c3a630c6456c9917c55ffce0f47ddc86f38eb9a2
riceman0000 <45267493+riceman0000@users.noreply.github.com>
2020-03-19 04:41:24 +0900

    ゆなのAnimation、動き変更

--- Yuna/Assets/otsuki/testMov.cs
+++ Yuna/Assets/otsuki/testMov.cs
@@ -6,6 +6,7 @@ namespace Yuna
 {
     public class testMov : ManagedMono
     {
+#if false
         private Animator anim = null;
         public float speed = 2.0f;
         // Start is called before the first frame update
@@ -50,5 +51,63 @@ namespace Yuna
             }
             transform.position += mov;
         }
+#endif
+
+#if true
+        private Animator anim;
+        [SerializeField] float speed = 0f;
+        string setAnimState = null;
+
+        private void Start()
+        {
+            anim = GetComponent<Animator>();
+        }
+        public override void MFixedUpdate()
+        {
+            Move();
+        }
+        void Move()
+        {
+            bool isWalk = false;
+            int direction = -1;
+            Vector3 move = Vector3.zero;
+            float correction = 1f;
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                correction = 0.75f;
+                move += Vector3.up;
+                direction = 2;
+                isWalk = true;
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                correction = 0.75f;
+                move += Vector3.down;
+                direction = 0;
+                isWalk = true;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                move += Vector3.right;
+                direction = 1;
+                transform.localScale = new Vector3(1, 1, 1);
+                isWalk = true;
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                move += Vector3.left;
+                direction = 1;
+                transform.localScale = new Vector3(-1,1,1);
+                isWalk = true;
+            }
+
+
+            anim.SetBool("isWalk", isWalk);
+            anim.SetInteger("direction", direction);
+
+            transform.position += move * speed * correction;
+        }
+#endif
     }
+
 }
